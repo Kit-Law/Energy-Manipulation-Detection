dcb03c290b8dfd0aba049470e6ea6b56c0265429
Kit <34525535+Kit-Law@users.noreply.github.com>
2022-05-11 12:48:06 +0100

    Commented TaskSolver.py

--- Src/Task Planer/TaskSolver.py
+++ Src/Task Planer/TaskSolver.py
@@ -6,8 +6,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 
+#findOptimalSchedule crates a linear program to find the most optimal solution
+#for what time each user's tasks should be run, acording to the early time,
+#late time, requierd energy for the task and the cost of energy at each hour
 def findOptimalSchedule(priceCurves, userTasks, curveNumber):
-    # Define the model
+    # Define the lp model (minimize to find the ceapest energy cost)
     model = LpProblem(name="scheduling-allocation", sense=LpMinimize)
 
     allVariables = []
@@ -15,24 +18,20 @@ def findOptimalSchedule(priceCurves, userTasks, curveNumber):
     #Create linear constraints
     for index, task in userTasks.iterrows():
         variables = []
-        for hour in range(0, task["Ready Time"]):
-            variables.append(LpVariable(name=(task["User & Task ID"] + "_hour" + str(hour)), 
-                lowBound= 0, upBound = 0, cat='Integer'))
-
-
+        
         for hour in range(task["Ready Time"], task["Deadline"] + 1):
+            #Define variables for all possible running times for the task
             variable = LpVariable(name=(task["User & Task ID"] + "_hour" + str(hour)), 
                 lowBound= 0, upBound = task["Maximum scheduled energy per hour"], cat='Integer')
+            #Add the cost of this hour of the task to the objective function buffer
             objectiveFunc.append(priceCurves[hour] * variable)
             variables.append(variable)
 
-        model += (lpSum(variables[task["Ready Time"] : task["Deadline"] + 1]) == task["Energy Demand"], task["User & Task ID"])
-
-        for hour in range(task["Deadline"] + 1, 24):
-            variables.append(LpVariable(name=(task["User & Task ID"] + "_hour" + str(hour)), lowBound= 0, upBound = 0, cat='Integer'))
-
+        #Add the constraint to make the sum of the variables equal the energy demand
+        model += (lpSum(variables) == task["Energy Demand"], task["User & Task ID"])
         allVariables.append(variables)
 
+    #Add the objective function
     model += lpSum(objectiveFunc)
 
     # Solve the optimization problem
@@ -42,6 +41,11 @@ def findOptimalSchedule(priceCurves, userTasks, curveNumber):
     print(f"status: {model.status}, {LpStatus[model.status]}")
     print(f"objective: {model.objective.value()}")
 
+    #Commented code shows the varible values
+    #for var in model.variables():
+    #    if var.value() != 0.0:
+    #        print(f"{var.name}: {var.value()}")
+
     #for var in [item for sublist in allVariables for item in sublist]:
     #    print(f"{var.name}: {var.value()}")
 
@@ -50,31 +54,37 @@ def findOptimalSchedule(priceCurves, userTasks, curveNumber):
 
     plotGraph(model, allVariables, curveNumber)
 
+#labelTasks labels each bar with it's task id
 def labelTasks(barPlot, tallestHieghts, label):
     for i,rect in enumerate(barPlot):
         if rect.get_height() != 0:
             plt.gca().text(rect.get_x() + 0.05 + rect.get_width()/2., tallestHieghts[i] + 0.1,
                     label, ha='center', va='bottom', rotation=90)
 
+#plotGraph plots the optimial times to run each task for the user
+#The graph is a bar chat that has stacked bars, with labels on each
+#for the task id.
 def plotGraph(model, allVariables, curveNumber):
+    #Initilize the metadata for the graph
     hours = [str(x) for x in range(0, 24)]
     pos = np.arange(len(hours))
     color_list = ['plum','lightpink','lightsteelblue','paleturquoise','lightskyblue']
     tallestHieghts = np.full(24, 0)
 
+    #Seperate the non zero values for the varibles into a 2D array for the bar heights
     for tasks in allVariables:
-        heights = []
-        for task in tasks:
-            if task.value() == None:
-                heights.append(0)
-            else:
-                heights.append(int(task.value()))
+        heights = [0] * 24
+        for i, task in enumerate(tasks):
+            if task.value() != None:
+                heights[i] = int(task.value())
         
+        #Plot the task heights along with task id label
         bar = plt.bar(pos, heights, color=color_list[int(tasks[0].name[4]) - 1], edgecolor='black',bottom=tallestHieghts)
         labelTasks(bar, tallestHieghts, tasks[0].name.split('_')[1][4:6])
 
         tallestHieghts += np.array(heights)
 
+    #Disply settings for the graph
     plt.xticks(pos, hours)
     plt.xlabel('Hour Of The Day')
     plt.ylabel('Energy Usage (kWH)')
@@ -90,6 +100,7 @@ def plotGraph(model, allVariables, curveNumber):
     plt.savefig('..\..\Output\Plots\\'+str(curveNumber)+'.png')
     plt.clf()
 
+#Main Program
 
 #Read the labeled priceing curves
 priceCurves = pd.read_csv('..\..\Output\TestingResults.txt', header=None)
@@ -99,6 +110,7 @@ priceCurves = np.array(priceCurves)
 userTasks = pd.read_excel('..\..\Input\COMP3217CW2Input.xlsx', sheet_name='User & Task ID',
     dtype={'User & Task ID': str, 'Ready Time': int, 'Deadline':int, 'Maximum scheduled energy per hour':int, 'Energy Demand':int})
 
+#Find the optimal solution for each curve
 for curveNumber in range(len(priceCurves)):
     if priceCurves[curveNumber][24] == 1:
         findOptimalSchedule(priceCurves[curveNumber][:-1], userTasks, curveNumber)
